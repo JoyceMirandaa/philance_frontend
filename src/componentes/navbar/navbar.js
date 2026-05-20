c17b7ae20b6bf2606c9c50b2eba399540ae04422
JoyceMirandaa <biorajoycem@gmail.com>
2026-05-20 17:17:55 -0300

    Add files via upload

---
+++ src/componentes/navbar/navbar.js
@@ -0,0 +1,45 @@
+function carregarNavbarUsuario() {
+    const html = `
+
+        <nav class="navbar nav-usuario">
+          <div class="logo"><a href="/src/index.html">Portal Usuário</a></div>
+          <ul class="nav-links">
+            <li><a href="/src/index.html">Home</a></li>
+            <li><a href="/src/pages/usuario/perfil.html">Meu Perfil</a></li>
+            <li><a href="/src/pages/usuario/vagas.html">Buscar Vagas</a></li>
+          </ul>
+        </nav>
+    `;
+    const container = document.getElementById('space-navbar');
+    if (container) container.innerHTML = html;
+}
+
+function carregarNavbarEmpresa() {
+    const html = `
+        <nav class="navbar nav-empresa">
+          <div class="logo"><a href="/src/index.html">Portal Empresa</a></div>
+          <ul class="nav-links">
+            <li><a href="/src/index.html">Home</a></li>
+            <li><a href="/src/pages/empresa/dashboard.html">Painel</a></li>
+            <li><a href="/src/pages/empresa/criar-vaga.html">Publicar Vaga</a></li>
+          </ul>
+        </nav>
+    `;
+    const container = document.getElementById('space-navbar');
+    if (container) container.innerHTML = html;
+}
+
+function carregarNavbarHome(){
+    const html = `
+        <nav class="navbar nav-home">
+            <div class="logo"><img src="/assets/imagens/PhilanceHome.png"> </div>
+            <ul class "nav-links>
+                <button><a href="/scr/pages/cadastro/freelancer/freelancerCadastro.html">Cadastro</a></button>
+                <button><a href="/scr/pages/cadastro/freelancer/freelancerCadastro.html">Login</a></button>
+            </nav>
+
+        </nav>
+    `;
+    const container = document.getElementById('space-navbar');
+    if (container) container.innerHTML = html;
+}
