8031c01945d2c1d0c3717978fdbfdf8e7099aba0
JoyceMirandaa <biorajoycem@gmail.com>
2026-06-12 17:21:16 -0300

    Delete philance_frontend-main directory

--- philance_frontend-main/README.md
+++
@@ -1,1 +0,0 @@
-# philance_frontend
--- philance_frontend-main/src/pages/Home/empresa/home.js
+++
@@ -1,7 +0,0 @@
-const dadosSalvos = localStorage.getItem("dadosUsuario");
-
-if (!dadosSalvos){
-  window.location.href = "index.html"
-}
-
-const usuario = JSON.parse(dadosSalvos)
